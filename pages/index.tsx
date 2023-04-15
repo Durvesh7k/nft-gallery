3ff49aa7b56a39f765a2b6b050bd10d510f95b9c
Durvesh7k <dschopade7k@gmail.com>
2023-04-15 16:25:34 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,125 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
+type nftType = {
+  description : string,
+  tokenUri: {
+    gateway: string
+  }
+}
+
+type nftType1 = {
+  contractMetadata: {
+    openSea: {
+      collectionName: string,
+      imageUrl: string
+    }
+  }
+}
+
+
 export default function Home() {
+  const [nfts, setNfts] = useState<{ name: string, image: string }[]>([]);
+  const [account, setAccount] = useState<string>('');
+  const [collectionAddress, setCollectionAddress] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const api_key = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
+
+  async function getNfts(): Promise<void> {
+    const options = {
+      method: 'GET',
+      url: `https://eth-mainnet.g.alchemy.com/nft/v2/${api_key}/getNFTs`,
+      params: { owner: `${account}`, withMetadata: 'true', pageSize: '100' },
+      headers: { accept: 'application/json' }
+    };
+
+    try {
+      setLoading(true)
+      const response = await axios.request(options)
+      console.log(response.data);
+      let nftsData: { name: string, image: string }[] = []
+      response.data.ownedNfts.map((nft: nftType1, i: Number) => {
+        nftsData.push({
+          name: nft.contractMetadata.openSea.collectionName,
+          image: nft.contractMetadata.openSea.imageUrl,
+        })
+      })
+      setNfts(nftsData)
+      setLoading(false)
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+
+  async function getNftsByCollection(): Promise<void> {
+    const options = {
+      method: 'GET',
+      url: `https://eth-mainnet.g.alchemy.com/nft/v2/${api_key}/getNFTsForCollection`,
+      params: {
+        contractAddress: `${collectionAddress}`,
+        withMetadata: 'true'
+      },
+      headers: { accept: 'application/json' }
+    };
+
+    try {
+      setLoading(true)
+      const response = await axios.request(options)
+      console.log(response.data);
+      let nftsData: { name: string, image: string }[] = []
+      response.data.nfts.map((nft: nftType, i: Number) => {
+        nftsData.push({
+          name: nft.description,
+          image: nft.tokenUri.gateway,
+        })
+      })
+      setNfts(nftsData)
+      setLoading(false)
+      console.log(nfts)
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='grid gap-6 justify-center'>
+      <div className='flex justify-center'>
+        <h1 className='text-2xl mt-4 font-semibold'>NFT Gallery</h1>
+      </div>
+      <div className='grid grid-cols-1 gap-3 justify-center text-black font-semibold'>
+        <div className='grid justify-center gap-3 '>
+          <input className='h-10 w-80 rounded-md outline-none p-4' onChange={e => setAccount(e.target.value)} placeholder='Enter the account address' />
+          <input className='h-10 w-80 rounded-md outline-none p-4' onChange={e => setCollectionAddress(e.target.value)} placeholder='Enter the collection address' />
+        </div>
+        <div className='flex gap-4 justify-center'>
+          <button onClick={getNfts} className='w-32 rounded-md h-10 bg-indigo-700 text-white hover:bg-indigo-500'>Byaddress</button>
+          <button onClick={getNftsByCollection} className='w-32 rounded-md h-10 bg-indigo-700 text-white hover:bg-indigo-500'>Bycollection</button>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {!loading ? (
+        <div className='grid gap-8 grid-cols-4 justify-center'>
+          {nfts.map((nft, i) => {
+            return (
+              <div className='grid justify-center w-60 h-40 rounded-md bg-white font-semibold' key={i}>
+                <img className='w-40 h-40' alt='Nft image' src={nft.image} />
+                <h2>{nft.name}</h2>
+              </div>
+            )
+          })}
+        </div>
+      ) : (
+        <div className='flex justify-center'>
+          <span>Loading.....</span>
+        </div>
+      )}
+    </div>
   )
 }
